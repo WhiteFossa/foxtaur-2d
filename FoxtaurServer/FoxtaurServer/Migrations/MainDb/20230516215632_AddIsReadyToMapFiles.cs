fc2476075f439f7824aeca579d4e7e9a1f374526
White Fossa <whitefossa@protonmail.com>
2023-05-19 01:20:31 +0200

    Added "Is ready" migration

---
+++ FoxtaurServer/FoxtaurServer/Migrations/MainDb/20230516215632_AddIsReadyToMapFiles.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoxtaurServer.Migrations.MainDb
+{
+    /// <inheritdoc />
+    public partial class AddIsReadyToMapFiles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReady",
+                table: "MapFiles",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
